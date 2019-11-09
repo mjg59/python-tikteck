c528abfefe75c38fce46647f4e46fa791213b8ba
Driftall <blakelloyd@hotmail.co.uk>
2019-11-09 02:32:29 +0000

    Bump version to 0.5 to include individual light control fix

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ import warnings
 
 dynamic_requires = []
 
-version = 0.4
+version = 0.5
 
 setup(
     name='tikteck',
-    version=0.4,
+    version=0.5,
     author='Matthew Garrett',
     author_email='mjg59@srcf.ucam.org',
     url='http://github.com/mjg59/python-tikteck',
